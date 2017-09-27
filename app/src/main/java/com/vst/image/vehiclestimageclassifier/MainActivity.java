c6e308a543ea8acb3646d44f0fa61c726dc5a38f
sooorajjj <sooorajjj@gmail.com>
2017-09-27 16:34:54 +0530

    Add: Navigation Drawer and Toolbar!

--- app/src/main/java/com/vst/image/vehiclestimageclassifier/MainActivity.java
+++ app/src/main/java/com/vst/image/vehiclestimageclassifier/MainActivity.java
@@ -3,11 +3,17 @@ package com.vst.image.vehiclestimageclassifier;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -38,7 +44,7 @@ import java.util.Map;
 
 import gun0912.tedbottompicker.TedBottomPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     public RequestManager mGlideRequestManager;
@@ -51,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(MainActivity.this);
 
         mGlideRequestManager = Glide.with(this);
 
@@ -58,8 +75,43 @@ public class MainActivity extends AppCompatActivity {
 
         setMultiShowButton();
 
-            uploadButton();
+        uploadButton();
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_main);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_upload) {
+            // Handle the camera action
+        } else if (id == R.id.nav_view_image) {
+
+        } else if (id == R.id.nav_download) {
+
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(MainActivity.this, "Share to SocialMedia is [WIP]\n",
+                    Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_about) {
+            Toast.makeText(MainActivity.this, "About Screen is [WIP]\n",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_main);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
     private void setMultiShowButton() {
 
